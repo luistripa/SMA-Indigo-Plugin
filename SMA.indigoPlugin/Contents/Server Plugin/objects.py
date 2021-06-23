638c1ceeddaf45db613e636b01c2e3ba4e56538c
Luis Tripa <54379665+luistripa@users.noreply.github.com>
2021-06-23 10:28:32 +0100

    Moved HomeManager object to objects.py. Added a few comments. Now discontinuing HomeManager support

--- SMA.indigoPlugin/Contents/Server Plugin/objects.py
+++ SMA.indigoPlugin/Contents/Server Plugin/objects.py
@@ -1,6 +1,11 @@
 from pymodbus.client.sync import ModbusTcpClient as ModbusClient
 from comms import Client
 
+from typing import List
+import socket
+from struct import pack
+from binascii import b2a_hex
+
 
 class Inverter:
     def __init__(self, device, host, port):
@@ -16,6 +21,10 @@ class Inverter:
     def disconnect(self):
         self.client.close()
 
+    def reconnect(self):
+        self.disconnect()
+        return self.connect()
+
     def update_states(self):
         indigo_states = []
         self.states = self.client.generate_states()
@@ -25,9 +34,12 @@ class Inverter:
 
 
 class InverterBundle:
+    """
+    This object is not operational yet
+    """
     def __init__(self, device):
         self.device = device
-        self.inverters = list()
+        self.inverters: List[Inverter] = list()
 
     def add_inverter(self, inverter: Inverter):
         self.inverters.append(inverter)
@@ -40,7 +52,7 @@ class InverterBundle:
         """
         Updates all bundle states.
         """
-        pass
+        raise NotImplementedError("Function update_all_states is not yet implemented")
 
     def get_count(self) -> int:
         return len(self.inverters)
@@ -49,3 +61,41 @@ class InverterBundle:
         for inverter in self.inverters:
             inverter.disconnect()
             inverter.connect()
+
+
+class HomeManager:
+    """
+    Home Manager support is being discontinued by this plugin. The values that are output are not correct and should
+    not be used.
+    """
+
+    def __init__(self, device):
+        self.device = device
+        self.mcastGroup = device.pluginProps['multicastGroup']
+        self.mcastPort = int(device.pluginProps['multicastPort'])
+        self.setup()
+
+    def hex2dec(self, s):
+        return int(s, 16)
+
+    def setup(self):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.sock.bind(('', self.mcastPort))
+        mreq = pack("4sl", socket.inet_aton(self.mcastGroup), socket.INADDR_ANY)
+        self.sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+
+    def getReading(self):
+        info = self.sock.recv(600)
+        info_ascii = b2a_hex(info)
+        powerFromGrid = self.hex2dec(info_ascii[64:72]) / 10
+        totalPowerFromGrid = self.hex2dec(info_ascii[80:96]) / 3600000
+        powerToGrid = self.hex2dec(info_ascii[104:112]) / 10
+        totalPowerToGrid = self.hex2dec(info_ascii[120:136]) / 3600000
+        return [{'key': 'powerFromGrid', 'value': powerFromGrid},
+                {'key': 'totalPowerFromGrid', 'value': totalPowerFromGrid},
+                {'key': 'powerToGrid', 'value': powerToGrid},
+                {'key': 'totalPowerToGrid', 'value': totalPowerToGrid}]
+
+    def close(self):
+        self.sock.close()
--- SMA.indigoPlugin/Contents/Server Plugin/plugin.py
+++ SMA.indigoPlugin/Contents/Server Plugin/plugin.py
@@ -1,51 +1,12 @@
 import indigo
-import socket
-from struct import pack
-from binascii import b2a_hex
 
-from objects import Inverter, InverterBundle
+from objects import Inverter, InverterBundle, HomeManager
 import traceback
 
 DISPLAY_NAME = 'Energy Meter'
 
 
 class Plugin(indigo.PluginBase):
-    class HomeManager:
-        """
-        Output values are not correct
-        """
-
-        def __init__(self, device):
-            self.device = device
-            self.mcastGroup = device.pluginProps['multicastGroup']
-            self.mcastPort = int(device.pluginProps['multicastPort'])
-            self.setup()
-
-        def hex2dec(self, s):
-            return int(s, 16)
-
-        def setup(self):
-            self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-            self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-            self.sock.bind(('', self.mcastPort))
-            mreq = pack("4sl", socket.inet_aton(self.mcastGroup), socket.INADDR_ANY)
-            self.sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
-
-        def getReading(self):
-            info = self.sock.recv(600)
-            info_ascii = b2a_hex(info)
-            powerFromGrid = self.hex2dec(info_ascii[64:72]) / 10
-            totalPowerFromGrid = self.hex2dec(info_ascii[80:96]) / 3600000
-            powerToGrid = self.hex2dec(info_ascii[104:112]) / 10
-            totalPowerToGrid = self.hex2dec(info_ascii[120:136]) / 3600000
-            return [{'key': 'powerFromGrid', 'value': powerFromGrid},
-                    {'key': 'totalPowerFromGrid', 'value': totalPowerFromGrid},
-                    {'key': 'powerToGrid', 'value': powerToGrid},
-                    {'key': 'totalPowerToGrid', 'value': totalPowerToGrid}]
-
-        def close(self):
-            self.sock.close()
-
     def __init__(self, pluginId, pluginDisplayName, pluginVersion, pluginPrefs):
         indigo.PluginBase.__init__(self, pluginId, pluginDisplayName, pluginVersion, pluginPrefs)
 
@@ -112,7 +73,7 @@ class Plugin(indigo.PluginBase):
             self.bundles[dev.name] = bundle
 
         elif dev.deviceTypeId == 'homeManager':
-            home_manager = self.HomeManager(dev)
+            home_manager = HomeManager(dev)
             self.homeManagers[dev.name] = home_manager
 
         else:
@@ -130,6 +91,10 @@ class Plugin(indigo.PluginBase):
     ###########################
 
     def update_inverters(self):
+        """
+        Updates the states of all Inverters.
+        If, for some reason, a ConnectionError is found, it attempts to reconnect the inverter
+        """
         for inv in self.inverters.values():
             try:
                 inv.update_states()
@@ -141,10 +106,18 @@ class Plugin(indigo.PluginBase):
                 inv.connect()  # Reconnect inverter
 
     def update_inverter_bundles(self):
+        """
+        Updates the states of all Inverter Bundles
+        """
         for bundle in self.bundles.values():
             bundle.update_all_states()
 
     def update_home_managers(self):
+        """
+        Updates all Home Managers.
+        Home Manager support is being discontinued by this plugin. The values that are output are not correct and should
+        not be used.
+        """
         for hm in self.homeManagers.values():
             try:
                 states = hm.getReading()
@@ -153,16 +126,25 @@ class Plugin(indigo.PluginBase):
                 indigo.server.log(traceback.print_exc(), type=DISPLAY_NAME)
 
     def reconnect_all(self):
+        """
+        Reconnects all devices
+        """
         indigo.server.log('Reconnecting all devices........%d devices' % len(self.devices))
         for inv in self.inverters.values():
-            if inv.connect():
+            if inv.reconnect():
                 indigo.server.log('    - {} --- OK'.format(inv.device.name), type=DISPLAY_NAME)
             else:
                 indigo.server.log('    - {} --- FAILED'.format(inv.device.name), type=DISPLAY_NAME)
 
     def reconnect_device(self, valuesDict, typeId):
-        indigo.server.log('Not yet implemented', type=DISPLAY_NAME)
+        """
+        Reconnects a specific device
+        """
+        indigo.server.log('Reconnect Device is not yet implemented', type=DISPLAY_NAME)
 
     def reconnect_bundle(self, valuesDict, typeId):
+        """
+        Reconnects all devices in a specific bundle
+        """
         bundle = indigo.devices[valuesDict['targetBundle']]
         bundle.reconnectAll()
